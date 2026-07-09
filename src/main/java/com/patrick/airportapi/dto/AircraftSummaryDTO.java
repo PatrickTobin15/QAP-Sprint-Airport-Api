93667677172e685f217ca20be27496596ee25fe7
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 10:03:45 -0230

    Update AircraftSummaryDTO.java

--- src/main/java/com/patrick/airportapi/dto/AircraftSummaryDTO.java
+++ src/main/java/com/patrick/airportapi/dto/AircraftSummaryDTO.java
@@ -1,1 +1,41 @@
+package com.patrick.airportapi.dto;
 
+public class AircraftSummaryDTO {
+
+    private Long id;
+    private String type;
+    private String airlineName;
+
+    public AircraftSummaryDTO() {
+    }
+
+    public AircraftSummaryDTO(Long id, String type, String airlineName) {
+        this.id = id;
+        this.type = type;
+        this.airlineName = airlineName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+}
