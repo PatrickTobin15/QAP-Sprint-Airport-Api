8eab92bb8560627458261e03d9bf0b5b283892d5
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 11:21:41 -0230

    Update City.java

--- src/main/java/com/patrick/airportapi/entity/City.java
+++ src/main/java/com/patrick/airportapi/entity/City.java
@@ -1,1 +1,57 @@
+package com.patrick.airportapi.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String state;
+    private Integer population;
+
+    public City() {
+    }
+
+    public City(String name, String state, Integer population) {
+        this.name = name;
+        this.state = state;
+        this.population = population;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+}
