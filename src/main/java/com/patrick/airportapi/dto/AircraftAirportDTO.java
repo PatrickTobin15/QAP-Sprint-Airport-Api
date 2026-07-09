590793bab099ba01f719ca6a9e06ab4f82d2ce1d
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 10:03:27 -0230

    Update AircraftAirportDTO.java

--- src/main/java/com/patrick/airportapi/dto/AircraftAirportDTO.java
+++ src/main/java/com/patrick/airportapi/dto/AircraftAirportDTO.java
@@ -1,1 +1,53 @@
+package com.patrick.airportapi.dto;
 
+import java.util.List;
+
+public class AircraftAirportsDTO {
+
+    private Long aircraftId;
+    private String type;
+    private String airlineName;
+    private List<AirportSummaryDTO> airports;
+
+    public AircraftAirportsDTO() {
+    }
+
+    public AircraftAirportsDTO(Long aircraftId, String type, String airlineName, List<AirportSummaryDTO> airports) {
+        this.aircraftId = aircraftId;
+        this.type = type;
+        this.airlineName = airlineName;
+        this.airports = airports;
+    }
+
+    public Long getAircraftId() {
+        return aircraftId;
+    }
+
+    public void setAircraftId(Long aircraftId) {
+        this.aircraftId = aircraftId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public List<AirportSummaryDTO> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<AirportSummaryDTO> airports) {
+        this.airports = airports;
+    }
+}
