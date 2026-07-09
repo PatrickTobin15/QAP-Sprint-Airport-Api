af4e9f1e77e3ef0d3f8aa1ebb9945b1f95a0c98b
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 11:21:26 -0230

    Update Airport.java

--- src/main/java/com/patrick/airportapi/entity/Airport.java
+++ src/main/java/com/patrick/airportapi/entity/Airport.java
@@ -1,1 +1,63 @@
+package com.patrick.airportapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "airports")
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    // belongs to one city
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "city_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private City city;
+
+    public Airport() {
+    }
+
+    public Airport(String name, String code, City city) {
+        this.name = name;
+        this.code = code;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
