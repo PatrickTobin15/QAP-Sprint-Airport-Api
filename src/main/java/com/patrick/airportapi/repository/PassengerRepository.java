06462672a792c4ca47c73abb1df76efa87f5d7d0
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 11:23:07 -0230

    Update PassengerRepository.java

--- src/main/java/com/patrick/airportapi/repository/PassengerRepository.java
+++ src/main/java/com/patrick/airportapi/repository/PassengerRepository.java
@@ -1,1 +1,8 @@
+package com.patrick.airportapi.repository;
 
+import com.patrick.airportapi.entity.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+    // findAll(Pageable) inherited - used for pagination
+}
