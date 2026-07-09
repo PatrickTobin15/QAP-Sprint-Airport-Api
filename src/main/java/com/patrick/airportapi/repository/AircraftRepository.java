9cad171b6cccf09b385627cd1a6234286bffa969
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 11:22:26 -0230

    Update AircraftRepository.java

--- src/main/java/com/patrick/airportapi/repository/AircraftRepository.java
+++ src/main/java/com/patrick/airportapi/repository/AircraftRepository.java
@@ -1,1 +1,7 @@
+package com.patrick.airportapi.repository;
 
+import com.patrick.airportapi.entity.Aircraft;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AircraftRepository extends JpaRepository<Aircraft, Long> {
+}
