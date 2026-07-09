84350100d2b0235837df16f28fb57051ff75db34
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 11:21:13 -0230

    Update Aircraft.java

--- src/main/java/com/patrick/airportapi/entity/Aircraft.java
+++ src/main/java/com/patrick/airportapi/entity/Aircraft.java
@@ -1,1 +1,78 @@
+package com.patrick.airportapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "aircraft")
+public class Aircraft {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private String airlineName;
+    private Integer numberOfPassengers;
+
+    // many-to-many with airports
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "aircraft_airport",
+            joinColumns = @JoinColumn(name = "aircraft_id"),
+            inverseJoinColumns = @JoinColumn(name = "airport_id")
+    )
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private List<Airport> airports = new ArrayList<>();
+
+    public Aircraft() {
+    }
+
+    public Aircraft(String type, String airlineName, Integer numberOfPassengers) {
+        this.type = type;
+        this.airlineName = airlineName;
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public Integer getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(Integer numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<Airport> airports) {
+        this.airports = airports;
+    }
+}
