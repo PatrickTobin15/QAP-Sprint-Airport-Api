2ca8fcdbe245e52f8923671fcce6c2846d1287a4
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 10:02:22 -0230

    Update CityController.java

--- src/main/java/com/patrick/airportapi/controller/CityController.java
+++ src/main/java/com/patrick/airportapi/controller/CityController.java
@@ -1,1 +1,54 @@
+package com.patrick.airportapi.controller;
 
+import com.patrick.airportapi.dto.CityAirportsDTO;
+import com.patrick.airportapi.entity.City;
+import com.patrick.airportapi.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cities")
+public class CityController {
+
+    private final CityService cityService;
+
+    @Autowired
+    public CityController(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+    @GetMapping
+    public List<City> getAllCities() {
+        return cityService.getAllCities();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<City> getCityById(@PathVariable Long id) {
+        return ResponseEntity.ok(cityService.getCityById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<City> createCity(@RequestBody City city) {
+        return ResponseEntity.ok(cityService.createCity(city));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<City> updateCity(@PathVariable Long id, @RequestBody City city) {
+        return ResponseEntity.ok(cityService.updateCity(id, city));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCity(@PathVariable Long id) {
+        cityService.deleteCity(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Q1: airports per city
+    @GetMapping("/airports-summary")
+    public List<CityAirportsDTO> getCitiesWithAirports() {
+        return cityService.getCitiesWithAirports();
+    }
+}
