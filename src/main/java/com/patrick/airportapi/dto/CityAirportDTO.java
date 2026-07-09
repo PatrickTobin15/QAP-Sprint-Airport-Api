be43db2a743fac2e25f9bea788a473e3c382d5cd
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 11:19:54 -0230

    Update CityAirportDTO.java

--- src/main/java/com/patrick/airportapi/dto/CityAirportDTO.java
+++ src/main/java/com/patrick/airportapi/dto/CityAirportDTO.java
@@ -1,1 +1,43 @@
+package com.patrick.airportapi.dto;
 
+import java.util.List;
+
+public class CityAirportsDTO {
+
+    private Long cityId;
+    private String cityName;
+    private List<AirportSummaryDTO> airports;
+
+    public CityAirportsDTO() {
+    }
+
+    public CityAirportsDTO(Long cityId, String cityName, List<AirportSummaryDTO> airports) {
+        this.cityId = cityId;
+        this.cityName = cityName;
+        this.airports = airports;
+    }
+
+    public Long getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Long cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public List<AirportSummaryDTO> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<AirportSummaryDTO> airports) {
+        this.airports = airports;
+    }
+}
