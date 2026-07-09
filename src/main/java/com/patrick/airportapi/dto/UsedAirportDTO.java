96eeef44f0ff48e298ba4a9cd6be28312b848f30
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 11:20:21 -0230

    Update UsedAirportDTO.java

--- src/main/java/com/patrick/airportapi/dto/UsedAirportDTO.java
+++ src/main/java/com/patrick/airportapi/dto/UsedAirportDTO.java
@@ -1,1 +1,51 @@
+package com.patrick.airportapi.dto;
 
+public class UsedAirportDTO {
+
+    private Long airportId;
+    private String name;
+    private String code;
+    private String cityName;
+
+    public UsedAirportDTO() {
+    }
+
+    public UsedAirportDTO(Long airportId, String name, String code, String cityName) {
+        this.airportId = airportId;
+        this.name = name;
+        this.code = code;
+        this.cityName = cityName;
+    }
+
+    public Long getAirportId() {
+        return airportId;
+    }
+
+    public void setAirportId(Long airportId) {
+        this.airportId = airportId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
