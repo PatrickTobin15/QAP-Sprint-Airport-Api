c454b97bf2f9141a17f36f8a279a2d21b4037cb3
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-09 11:22:38 -0230

    Update AirportRepository.java

--- src/main/java/com/patrick/airportapi/repository/AirportRepository.java
+++ src/main/java/com/patrick/airportapi/repository/AirportRepository.java
@@ -1,1 +1,12 @@
+package com.patrick.airportapi.repository;
 
+import com.patrick.airportapi.entity.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AirportRepository extends JpaRepository<Airport, Long> {
+
+    // airports for one city
+    List<Airport> findByCity_Id(Long cityId);
+}
